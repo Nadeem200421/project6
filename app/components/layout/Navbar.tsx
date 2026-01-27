30f74b25745c1bf9ab09ff1504edc1da32f55cd0
Nadeem200421 <mohammednadeemhussain8@gmail.com>
2026-01-27 19:19:41 +0530

    Enhance Navbar menu button with additional spans
    Added three span elements for styling the menu button.

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -48,7 +48,9 @@ export default function Navbar() {
           aria-expanded={menuOpen}
           onClick={() => setMenuOpen(!menuOpen)}
         >
-          {menuOpen ? "✖" : "☰"}
+          <span />
+          <span />
+          <span />
         </button>
 
         {/* Menu */}
